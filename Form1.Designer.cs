39596c60a5b588472e0dcc2c10464081435e9fee
Daniel Bond <dbond04@gmail.com>
2021-09-18 01:31:28 -0400

    initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Excel_To_CSV_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.browse = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(491, 12);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Browse...";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(491, 41);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 1;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(429, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Save As:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(359, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(433, 41);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(51, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(578, 75);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.browse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Excel to CSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Excel_To_CSV_Converter
+{
+    public partial class Form1 : Form
+    {
+        private string newName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            // defines the parameters of the file browsing fucntion. 
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog1.Title = "Browse Files";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.DefaultExt = "xlsx";
+            openFileDialog1.Filter = "Excel Files (.xlsx)|*.xlsx"; // searches specifically for applicable file types
+            openFileDialog1.InitialDirectory = path + "\\Downloads\\"; //Sets default to the local download folder
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+
+            if ((this.textBox1.Text != null) && (this.textBox1.Text != ""))
+            {
+                if (MessageBox.Show("Would you like to set saved path name to default?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                    var extension = Path.GetExtension(this.textBox1.Text);
+                    if (string.Equals(extension, ".xls", StringComparison.OrdinalIgnoreCase) || string.Equals(extension, ".xlsx", StringComparison.OrdinalIgnoreCase)) {
+                        this.textBox2.Text = this.textBox1.Text.Replace(extension, ".csv");
+                    } else
+                    {
+                        MessageBox.Show("Invalid Path", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            if ((this.textBox1.Text != null) && (File.Exists(this.textBox1.Text)))
+            {
+                // this section grabs runs a test on the textbox for save as, sets a default if applicable (left blank)
+                if (this.textBox2.Text == null)
+                {
+                    DateTime today = DateTime.Today;
+                    string date = today.ToString("MM/dd/yyyy");
+                    date = date.Replace("/", "");
+                    string final = "LifeTest" + date + ".50093.csv";
+                    newName = this.textBox1.Text.Replace("MetLife.xlsx", final);
+                } else
+                {
+                    newName = this.textBox2.Text;
+                }
+                Console.WriteLine(newName);
+
+                // get the value in the save as text box to write the file.
+                if (this.textBox2.Text == string.Empty || Directory.Exists(Path.GetDirectoryName(this.textBox2.Text)))
+                {
+                    try
+                    {
+                        Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                        Microsoft.Office.Interop.Excel.Workbook workbook = app.Workbooks.Open(this.textBox1.Text);
+                        workbook.SaveAs(newName, Microsoft.Office.Interop.Excel.XlFileFormat.xlCSVWindows);
+                        workbook.Close();
+                        app.Quit();
+                        Process.Start(Path.GetDirectoryName(this.textBox2.Text));
+
+                    } catch
+                    {
+                        MessageBox.Show("No action completed.");
+                    }
+                    
+                } else
+                {
+                    MessageBox.Show("Save file directory does not exist!");
+                }
+            } else
+            {
+                MessageBox.Show("File not found.");
+            }
+        }
+
+        // clears out all value in both text boxes - button. 
+        private void clear_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = string.Empty;
+            this.textBox2.Text = string.Empty;
+        }
+
+        #region helpers
+        public string fileName(object sender, EventArgs e)
+        {
+            string newName = this.textBox1.Text.Replace(".xlsx", ".csv");
+            return newName;
+        }
+
+        #endregion
+    }
+}
